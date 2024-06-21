f3d6ed677e35c9dbcb81e4a8ab5307a630ddc9c9
gianlucaKun <gianlucakun95@gmail.com>
2024-06-21 19:18:16 +0200

    aggiunta info iosapp in appunti

---
+++ Appunti/test_iosapp/App.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, Button, StyleSheet } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [scannedData, setScannedData] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setScannedData(data);
+    alert(`Codice a barre con tipo ${type} e dati ${data} è stato scansionato!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Richiesta di autorizzazione della fotocamera</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>Nessun accesso alla fotocamera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+      {scannedData ? <Text>Dati scansionati: {scannedData}</Text> : null}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
