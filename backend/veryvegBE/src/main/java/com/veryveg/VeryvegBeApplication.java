37db0558a356c1f13ba0503d01973d418155b587
gianlucaKun <gianlucakun95@gmail.com>
2024-06-21 18:58:29 +0200

    creato backend principale veryveg

---
+++ backend/veryvegBE/src/main/java/com/veryveg/VeryvegBeApplication.java
@@ -0,0 +1,13 @@
+package com.veryveg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeryvegBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeryvegBeApplication.class, args);
+	}
+
+}
