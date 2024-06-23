ea712031c8438ddd0d4f43734d9192ff002215a9
Andriy <90765360+Takapirina@users.noreply.github.com>
2024-06-23 15:15:53 +0200

    mi sono messo un pò a giocare

--- frontend/pages/home.tsx
+++ frontend/pages/home.tsx
@@ -1,14 +1,27 @@
 // HomeScreen.tsx
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import AggiungiProdotto from '../components/forms/formAddProduct';
 
 
 const HomeScreen: React.FC = () => {
   return (
-    <View>
+    <View style={style.body}>
       <Text>Home Screen</Text>
+      <Text>bla bla bla</Text>
     </View>
   );
 };
 
 export default HomeScreen;
+
+const style = StyleSheet.create({
+  body : {
+    marginTop : 50,
+    height : '100%',
+    backgroundColor: '#343434',
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
--- frontend/pages/prova.tsx
+++ frontend/pages/prova.tsx
@@ -1,95 +1,102 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, Button, StyleSheet, FlatList, Alert } from 'react-native';
+import { Text, View, Button, StyleSheet, Alert } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
-import prodotti from '../other/datiRandom.json'
-
-import { RootStackParamList } from '../App';
-import { StackNavigationProp } from '@react-navigation/stack';
-
-type ProveScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Prove'>;
-
-type Props = {
-    navigation: ProveScreenNavigationProp;
-  };
+import prodotti from '../other/datiRandom.json';
+import AggiungiProdotto from '../components/forms/formAddProduct';
+import BottoneCustom from '../components/custom/buttonCustom/buttonCutom';
 
 export default function Prove() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
-  const [scannedData, setScannedData] = useState('');
-  const [isScanning, setIsScanning] = useState(false);
+    const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+    const [scanned, setScanned] = useState(false);
+    const [scannedData, setScannedData] = useState<string>('');
+    const [isScanning, setIsScanning] = useState(false);
+    const [showAddProduct, setShowAddProduct] = useState(false);
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
+    const annulla = () => {
+        setShowAddProduct(false);
+    }
 
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    setScannedData(data);
-    setIsScanning(false);
+    useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
 
-    // Cerca il prodotto corrispondente nel JSON
-    const prodotto = prodotti.prodotti.find(p => p.codice === data);
-    const nomeProdotto = prodotto ? prodotto.nome : 'Prodotto non trovato';
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        setScannedData(data);
+        setIsScanning(false);
 
-    Alert.alert(
-      'Codice a barre scansionato!',
-      `Codice a barre con tipo ${type} e dati ${data} è stato scansionato! Nome prodotto: ${nomeProdotto}`
-    );
-  };
+        // Cerca il prodotto corrispondente nel JSON
+        const prodotto = prodotti.prodotti.find(p => p.codice === data);
+        const nomeProdotto = prodotto ? prodotto.nome : null;
 
-  if (hasPermission === null) {
-    return <Text>Richiesta di autorizzazione della fotocamera</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>Nessun accesso alla fotocamera</Text>;
-  }
+        if (nomeProdotto) {
+            Alert.alert(
+                'Codice a barre scansionato!',
+                `Codice a barre con tipo ${type} e dati ${data} è stato scansionato! Nome prodotto: ${nomeProdotto}`
+            );
+        } else {
+            Alert.alert(
+                'Prodotto non trovato',
+                `Codice a barre con tipo ${type} e dati ${data} non è stato trovato. Vuoi aggiungere questo prodotto?`,
+                [
+                    { text: 'No', onPress: () => console.log('Aggiunta prodotto annullata'), style: 'cancel' },
+                    { text: 'Sì', onPress: () => setShowAddProduct(true) },
+                ]
+            );
+        }
+    };
 
-  return (
-    <View style={styles.container}>
-      {isScanning ? (
-        <BarCodeScanner
-          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={StyleSheet.absoluteFillObject}
-        />
-      ) : (
-        <>
-          <Button title="Scansiona un prodotto" onPress={() => setIsScanning(true)} />
-          {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-          {scannedData ? <Text>Dati scansionati: {scannedData}</Text> : null}
-          <FlatList
-            data={prodotti.prodotti}
-            keyExtractor={item => item.codice}
-            renderItem={({ item }) => (
-              <View style={styles.item}>
-                <Text>Codice: {item.codice}</Text>
-                <Text>Nome: {item.nome}</Text>
-              </View>
+    if (hasPermission === null) {
+        return <Text>Richiesta di autorizzazione della fotocamera</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>Nessun accesso alla fotocamera</Text>;
+    }
+
+    return (
+        <View style={styles.container}>
+            {isScanning ? (
+                <>
+                    <BarCodeScanner
+                        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                        style={StyleSheet.absoluteFillObject}
+                    />
+                    <BottoneCustom testo={'x'} onPress={() => setIsScanning(false)} stile={styles.exitButton}/>
+                </>
+            ) : (
+                <>
+                    <Button title="Scansiona un prodotto" onPress={() => setIsScanning(true)} />
+                    {scanned && <Button title={'Scansiona di nuovo'} onPress={() => setScanned(false)} />}
+                    {scannedData ? <Text>Dati scansionati: {scannedData}</Text> : null}
+                    {showAddProduct && <AggiungiProdotto codice={scannedData} event={annulla} />}
+                </>
             )}
-          />
-        </>
-      )}
-    </View>
-  );
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop:50
-  },
-  item: {
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 50,
+    },
+    item: {
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+    },
+    exitButton : {
+        backgroundColor: 'red',
+        position: 'absolute',
+        bottom: 30,
+        right: 30
+    }
 });
-
-
 
 /*
 import React, { useState, useEffect } from 'react';
